2193e0bdaf97bb46dd764684a91b5a4825b649ce
Kilder Montinegro <kilder.vsp@hotmail.com>
2022-08-10 23:24:58 -0300

    feat: :lipstick: add dark theme

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ void main() {
   runApp(
     GetMaterialApp(
       title: "Application",
+      theme: ThemeData.dark(),
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
     ),
