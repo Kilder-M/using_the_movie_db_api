39176b4241ea3886ec3406321321e78e7e49d84f
Kilder Montinegro <kilder.vsp@hotmail.com>
2022-08-10 23:25:18 -0300

    feat: :sparkles: new url add

--- lib/app/data/api.dart
+++ lib/app/data/api.dart
@@ -1,4 +1,7 @@
 class API {
+  static String requestMovieImage(String imageUrl) =>
+      'https://image.tmdb.org/t/p/w500/$imageUrl';
+
   static String requestMovie(int id) => '/movie/$id';
 
   static String requestMoviesSimilar(int id) => '/movie/$id/similar';
