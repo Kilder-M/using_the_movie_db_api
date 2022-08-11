689336ff8ced4018ef3e22f331280542dfe8471b
Kilder Montinegro <kilder.vsp@hotmail.com>
2022-08-11 16:33:38 -0300

    feat: :sparkles: ErrorBuilder when request image come error

--- lib/app/widgets/image_network_widget.dart
+++ lib/app/widgets/image_network_widget.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 
 Widget imageNetworkWidget(String url) {
   return (Uri.tryParse(url)!.isAbsolute)
-      ? Image.network(url)
+      ? Image.network(
+          url,
+          errorBuilder: (context, error, stackTrace) =>
+              const Icon(Icons.report_problem_outlined),
+        )
       : const Icon(Icons.report_problem_outlined);
 }
