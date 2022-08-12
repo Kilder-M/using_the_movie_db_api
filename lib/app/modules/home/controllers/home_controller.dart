848fa340065a4c437208ae1ee16fe991db404388
Kilder Montinegro <kilder.vsp@hotmail.com>
2022-08-11 22:36:31 -0300

    feat: :sparkles: home controller done

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,21 +1,26 @@
 import 'package:get/get.dart';
 import 'package:using_the_movie_db_api/app/data/dao/dio_impl.dart';
+import 'package:using_the_movie_db_api/app/data/dao/genre_impl.dart';
 import 'package:using_the_movie_db_api/app/data/dao/movie_impl.dart';
 import 'package:using_the_movie_db_api/app/data/dao/similar_movies_impl.dart';
 import 'package:using_the_movie_db_api/app/domain/models/movie_model.dart';
 import 'package:using_the_movie_db_api/app/domain/models/similar_movies_model.dart';
+import 'package:using_the_movie_db_api/app/domain/services/genre_service.dart';
 import 'package:using_the_movie_db_api/app/domain/services/movie_service.dart';
 import 'package:using_the_movie_db_api/app/domain/services/similar_movies_service.dart';
 
 class HomeController extends GetxController {
   final _movieService = MovieService(MovieImpl(DioImpl()));
+  final _genderService = GenreService(GenreImpl(DioImpl()));
   final _similarMoviesService =
       SimilarMoviesService(SimilarMoviesImpl(DioImpl()));
 
   var movie = Movie().obs;
   var similarMoviesList = <SimilarMovies>[].obs;
+  var genrelist = <Genre>[].obs;
   var isTapFavoriteIcon = false.obs;
   HomeController() {
+    getGenreList();
     getMovie();
     getSimilarMoviesList();
   }
@@ -27,5 +32,9 @@ class HomeController extends GetxController {
   Future<List<SimilarMovies>> getSimilarMoviesList() async {
     return similarMoviesList.value =
         await _similarMoviesService.getList(324786);
+  }
+
+  Future<List<Genre>> getGenreList() async {
+    return genrelist.value = await _genderService.getList();
   }
 }
